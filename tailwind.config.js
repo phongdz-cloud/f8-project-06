e1d8191b4f456dea18e6de0a862ccbfed5885df3
Phong Ho Hoai <phonghh@unit.com.vn>
2024-01-07 23:02:30 +0700

    section hero

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-custom": "#a32eff",
+        "gray-custom": "#5E6085",
+      },
+    },
+  },
+};
